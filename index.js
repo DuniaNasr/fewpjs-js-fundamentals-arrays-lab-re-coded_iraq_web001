0b4f7b53dac6a5abf408483e4702badb4c03d938
Dunia Nasr <dunia.nasr777@gmail.com>
2020-05-13 11:54:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const append =['Milo','Otis','Garfield','Odie'];
 const prepend =['Milo','Otis','Garfield','Odie'];
-const removeLast =['Milo','Otis','Garfield'];
-const removeFirst=['Otis','Garfield','Odie'];
+const removeLast =['Milo','Otis','Garfield','Odie'];
+const removeFirst=['Milo','Otis','Garfield','Odie'];
